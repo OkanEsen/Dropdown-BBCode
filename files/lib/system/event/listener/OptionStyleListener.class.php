55b09deea4a4dc0c23a296078c356a02edf2df7f
Okan Esen <pixel.b@gmail.com>
2011-07-14 21:28:05 +0200

    - updated license to lgpl

--- files/lib/system/event/listener/OptionStyleListener.class.php
+++ files/lib/system/event/listener/OptionStyleListener.class.php
@@ -5,7 +5,7 @@ require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
 /**
  * @author	Okan "[PixeL]" Esen
  * @copyright	2011 www.okan-esen.de
- * @license	Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	de.okanesen.wcf.data.message.bbcode.dropdown
  */
 class OptionStyleListener implements EventListener {
